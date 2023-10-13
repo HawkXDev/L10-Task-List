87b8bb30e7e7ac4975441cabb643f1b027cb5d85
sokolserg <sergsokolkin@gmail.com>
2023-10-13 08:17:21 +0300

    31. Sessions, Errors & Flash Messages; session()->has(); session()->get(); @error; redirect()->route()->with();

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,9 +2,16 @@
 
 @section('title', 'Add Task')
 
-@section('content')
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0.9rem;
+        }
+    </style>
+@endsection
 
-    {{-- {{ $errors }} --}}
+@section('content')
 
     <form action="{{ route('task.store') }}" method="POST">
         @csrf
@@ -12,14 +19,26 @@
             <label for="title">Title</label>
             <input type="text" id="title" name="title">
         </div>
+        @error('title')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
         <div>
             <label for="description">Description</label>
             <textarea id="description" name="description" rows="5"></textarea>
         </div>
+        @error('description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
         <div>
             <label for="long_description">Long Description</label>
             <textarea id="long_description" name="long_description" rows="10"></textarea>
         </div>
+        @error('long_description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
         <button type="submit">Submit</button>
     </form>
 
