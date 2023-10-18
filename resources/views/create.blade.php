760141504ccd9c4b1dda77bea87a8b7e848a17b6
sokolserg <sergsokolkin@gmail.com>
2023-10-18 18:59:37 +0300

    36. Reusing Blade Code - Subviews; form.blade.php instead create and update;

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,45 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Add Task')
-
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.9rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-
-    <form action="{{ route('task.store') }}" method="POST">
-        @csrf
-        <div>
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ old('title') }}">
-        </div>
-        @error('title')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <div>
-            <label for="description">Description</label>
-            <textarea id="description" name="description" rows="5">{{ old('description') }}</textarea>
-        </div>
-        @error('description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <div>
-            <label for="long_description">Long Description</label>
-            <textarea id="long_description" name="long_description" rows="10">{{ old('long_description') }}</textarea>
-        </div>
-        @error('long_description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <button type="submit">Submit</button>
-    </form>
-
+    @include('form')
 @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,46 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Task')
-
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.9rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-
-    <form action="{{ route('task.update', ['task' => $task->id]) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ $task->title }}">
-        </div>
-        @error('title')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <div>
-            <label for="description">Description</label>
-            <textarea id="description" name="description" rows="5">{{ $task->description }}</textarea>
-        </div>
-        @error('description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <div>
-            <label for="long_description">Long Description</label>
-            <textarea id="long_description" name="long_description" rows="10">{{ $task->long_description }}</textarea>
-        </div>
-        @error('long_description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
-
-        <button type="submit">Edit Task</button>
-    </form>
-
+    @include('form', ['task' => $task])
 @endsection
